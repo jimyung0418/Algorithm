e78880a9ae4c7ad787ed3d8d7d0b5a48e4e3c5f6
jimyung0418 <144213900+jimyung0418@users.noreply.github.com>
2024-02-23 17:46:30 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 65.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 1;
+        
+        if (num1 == num2) {
+            return answer;
+        } else {
+            return -1;
+        }
+    }
+}
