3fbf4a16866a9cd21755bd7be0eb150ad64b1cc5
jimyung0418 <144213900+jimyung0418@users.noreply.github.com>
2024-03-06 17:40:18 +0900

    [level 0] Title: 외계어 사전, Time: 0.05 ms, Memory: 69.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(String[] spell, String[] dic) {
+        int answer = 2;
+
+        for (int i = 0; i < dic.length; i++) {
+            int contains = 0;
+            for (int j = 0; j < spell.length; j++) {
+                if (dic[i].contains(spell[j])) {
+                    contains++;
+                }
+            }
+            if (contains == spell.length) {
+                answer -= 1;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
