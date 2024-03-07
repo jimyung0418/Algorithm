51c8cca53c3f11ac9bfd483bbde4e5c67696421e
jimyung0418 <144213900+jimyung0418@users.noreply.github.com>
2024-03-07 23:12:04 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -2,11 +2,17 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        if (n <= 7) {
-            answer = 1;
-        } else if (n % 7 == 0) {
+        // if (n <= 7) {
+        //     answer = 1;
+        // } else if (n % 7 == 0) {
+        //     answer = n / 7;
+        // } else if (n > 7 && n % 7 != 0) {
+        //     answer = n / 7 + 1;
+        // }
+        
+        if (n % 7 == 0) {
             answer = n / 7;
-        } else if (n > 7 && n % 7 != 0) {
+        } else {
             answer = n / 7 + 1;
         }
         
