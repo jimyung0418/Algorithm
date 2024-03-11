08364f8f77eac49cd1cb3b8454d480c92f656b25
jimyung0418 <144213900+jimyung0418@users.noreply.github.com>
2024-03-11 16:48:59 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.39 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {        
+        Arrays.sort(numbers);
+        int positive = numbers[numbers.length - 1] * numbers[numbers.length - 2];
+        int negative = numbers[0] * numbers[1];
+        
+        
+        return Math.max(positive, negative);
+    }
+}
