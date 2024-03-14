ef4d0a341c989b8e3b6e49b81fd01b5309f92c4d
jimyung0418 <144213900+jimyung0418@users.noreply.github.com>
2024-03-14 20:18:47 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                list.add(i);
+            }
+        }
+
+        int[] answer = new int[list.size()];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
