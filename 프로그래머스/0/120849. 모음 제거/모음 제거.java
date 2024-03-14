9bfb7b701a57f61b47a0550ebad42a9affadf2e1
jimyung0418 <144213900+jimyung0418@users.noreply.github.com>
2024-03-14 19:58:28 +0900

    [level 0] Title: 모음 제거, Time: 0.03 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            if (my_string.charAt(i) != 'a' && my_string.charAt(i) != 'e' && my_string.charAt(i) != 'i' && my_string.charAt(i) != 'o' && my_string.charAt(i) != 'u') {
+                sb.append(my_string.charAt(i));
+            }
+        }
+        
+        answer = sb.toString();
+        
+        return answer;
+    }
+}
