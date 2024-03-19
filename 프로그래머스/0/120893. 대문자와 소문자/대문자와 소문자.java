e911e34131d409f941ce8b85ad5fb182957b5ed1
jimyung0418 <144213900+jimyung0418@users.noreply.github.com>
2024-03-19 20:57:42 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.68 ms, Memory: 67.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        char[] arr = my_string.toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (Character.isUpperCase(arr[i])) {
+                arr[i] = Character.toLowerCase(arr[i]);
+            } else {
+                arr[i] = Character.toUpperCase(arr[i]);
+            }
+        }
+        
+        answer = new String(arr);
+        
+        return answer;
+    }
+}
