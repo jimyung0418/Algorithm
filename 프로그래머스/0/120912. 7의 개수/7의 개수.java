a0437295ecee6db2bb108bdfebce35ae3fc2b9b0
jimyung0418 <144213900+jimyung0418@users.noreply.github.com>
2024-04-03 16:27:46 +0900

    [level 0] Title: 7의 개수, Time: 14.39 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        String str = "";
+
+        for (int i = 0; i < array.length; i++) {
+            str += array[i];
+        }
+
+        for (int j = 0; j < str.length(); j++) {
+            if (str.charAt(j) == '7') {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
