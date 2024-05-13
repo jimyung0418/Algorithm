0f0b74c19f0938e45bd15f986af3a484a3d11ea0
jimyung0418 <144213900+jimyung0418@users.noreply.github.com>
2024-05-13 11:54:18 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -1,22 +1,15 @@
-import java.util.*;
-
 class Solution {
     public String solution(String[] id_pw, String[][] db) {
-        String answer = "login";
-        Map<String, String> map = new HashMap<>();
-        
-        for (int i = 0; i < db.length; i++) {
-            map.put(db[i][0], db[i][1]);
-        }
-        
-        if (map.containsKey(id_pw[0])) {
-            if (map.get(id_pw[0]).equals(id_pw[1])) {
-                return answer;
-            } else {
+        String answer = "";
+
+        for(int i = 0; i < db.length; i++){
+            if(db[i][0].equals(id_pw[0])){
+                if(db[i][1].equals(id_pw[1])){
+                    return "login";
+                }
                 return "wrong pw";
             }
-        } else {
-            return "fail";
         }
+        return "fail";
     }
 }
